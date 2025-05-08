50672a46ae687471d4c5ed6d75888e0bc2103a68
adrianneds <solisadrianned17@gmail.com>
2025-05-08 21:33:59 +0800

    feat: added function to retrieve org members with late payments

--- api/orgController.js
+++ api/orgController.js
@@ -7,13 +7,49 @@ const orgInfo = async (req, res) => {
 };
 
 // TO DO: view members of an organization (with filtering)
-const memberInfo = async (req, res) => {
+const orgMembers = async (req, res) => {
+
+    let committee = req.body.committee;
+    let role = req.body.role;
+    let status = req.body.status;
+    let gender = req.body.gender;
+    let degree_program = req.body.degree_program;
+
+    const query = 
+    `SELECT * FROM studentorg.member_${user}` ;
+
     const [rows] = await pool.query("SELECT * FROM ispartof_" + user);
     res.send(rows)
 };
 
-// TO DO: view members with unpaid fees for a given semester
+// TO DO: view members with late payments for a given semester/AY
+const orgUnpaidMembers = async (req, res) => {
+    
+    let semester = req.query.semester;                  // need to pass to query
+    let academic_year = req.query.academic_year;            
+
+    const query =
+    `SELECT student_number, member_name, fee_id, due_date, payment_date, payment_status 
+    FROM studentorg.pays_${user} NATURAL JOIN studentorg.member_${user}
+    AND semester = ${semester} AND academic_year = ${academic_year}
+    AND payment_status = 'Paid' AND payment_date > due_date`;
+
+    const [rows] = await pool.query(query);
+    res.send(rows)
+};
 
 // TO DO: view members of a specific committee
 
+
 export {orgInfo}
+// SELECT c.student_number, c.member_name, c.gender, c.degree_program, a.recent_status_date,
+// b.committee
+// FROM
+//     (SELECT student_number, MAX(date_of_status_update) as recent_status_date
+//     FROM is_part_of WHERE organization_id = @organization_id
+//     GROUP BY is_part_of.student_number) AS a
+// JOIN is_part_of AS b 
+// ON (a.recent_status_date = b.date_of_status_update AND a.student_number = b.student_number)
+// JOIN member AS c
+// ON (b.student_number = c.student_number)
+// WHERE committee = @committee;
