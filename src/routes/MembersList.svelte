ed8df49619a0b0d63ad5c4aaa50c85286ecbed57
adrianneds <solisadrianned17@gmail.com>
2025-05-10 12:01:01 +0800

    chore: added batch in member filter options

--- src/routes/MembersList.svelte
+++ src/routes/MembersList.svelte
@@ -180,6 +180,25 @@
         </ul>
     </div>
 
+    <button id="batchDropdown" data-dropdown-toggle="dropdown6" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+        Batch
+    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+    </svg>
+    </button>
+        <div id="dropdown6" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-40 dark:bg-gray-700">
+        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+        { #each batches as batch}
+        <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" on:click={() => filterQuery('batch',batch)}>{batch}</a>
+        </li>
+        { /each }
+        <li>
+            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white" on:click={() => filterQuery('batch','')}> Any </a>
+        </li>
+        </ul>
+    </div>
+
 
     </div>
 
@@ -250,7 +269,7 @@
   h1 {
     color: var(--text-primary);
   }
-  #genderDropdown, #statusDropdown, #degreeProgramDropdown, #committeeDropdown, #roleDropdown {
+  #genderDropdown, #statusDropdown, #degreeProgramDropdown, #committeeDropdown, #roleDropdown, #batchDropdown {
     color: black;
     background-color: var(--text-primary)
   }
